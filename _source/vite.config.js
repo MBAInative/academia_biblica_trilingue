29369282f8677e3c80b6c090cc7f66ebf3d2cb46
RamonParrondo <lbt00001@live.com>
2026-01-17 17:22:50 +0100

    Switch to relative paths ./ AND no htaccess

--- _source/vite.config.js
+++ _source/vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   build: {
     outDir: '../',
     emptyOutDir: false, // Important: don't delete .git or _source!
