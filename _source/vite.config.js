8a64aaf55934cabac49ce91eac63b922a496f1b3
RamonParrondo <lbt00001@live.com>
2026-01-17 17:06:48 +0100

    Restructure project: Move source to _source, put build in root

--- _source/vite.config.js
+++ _source/vite.config.js
@@ -5,6 +5,10 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: '../',
+    emptyOutDir: false, // Important: don't delete .git or _source!
+  },
   plugins: [react()],
   resolve: {
     alias: {
